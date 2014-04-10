ff686c69fc051740d4de512d21bc374e4357f8e2
Mark Harris <mark@sandlot>
2014-04-09 22:55:40 -0400

    simplify the form

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -40,16 +40,12 @@ class Idea < ActiveRecord::Base
 		end
 
 		state :proposed do
-			validates_presence_of :problem, :solution, :customer_segments, :high_level_concept, :launchpad_sponsor, :unique_value_prop
+			validates_presence_of :problem, :solution, :customer_segments, :high_level_concept 
 			#validates :score, :numericality => {:greater_than => 5, :message => '-- Ideas must have more than 5 votes to be considered'}
 		end
 
 		state :in_development do 
-			validates_presence_of :unique_value_prop, :key_metrics, :cost_structure, :revenue_streams
-		end
-
-		state :done do
-			validates_presence_of :early_adopters, :channels
+			validates_presence_of :unique_value_prop, :key_metrics
 		end
 
 	end
