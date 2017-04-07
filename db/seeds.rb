b4cf9ea19ecdea3324704dfc1505311a19a29161
Mark Harris <mharris@covermymeds.com>
2017-04-07 11:14:19 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 new_users = {
-  'Anna Harper Hess' => 'ahess',
-  'Dmitri Kazanski' => 'dkazanski',
-  'Eren Alasyali' => 'ealasyali',
-  'Heather Harmon' => 'hharmon',
-  'Jason Koeppe' => 'jkoeppe',
-  'Mark Brower' => 'mbrower',
-  'Mark Szolosi' => 'mszolosi',
-  'Matt Van Ormer' => 'mvanormer',
-  'Oksana Shmaliy' => 'oshmaliy',
-  'Russell Garrison' => 'rgarrison',
-  'Ty Henkaline' => 'thenkaline',
-  'Maureen Pechinak' => 'mpechinak'
+  'Mark Harris' => 'mharris'
 }
 
 new_users.each do |key, value|
-  u = User.new(name: key, email: "#{value}@manta.com", password: 'password', launchpad: true)
+  u = User.new(name: key, email: "#{value}@covermymeds.com", password: 'password', launchpad: true)
   if u.save
     puts "user #{key} created"
   else
